93f8d3dc34d79287b1587ed186e0db14c16d845a
paha <karandash5410@gmail.com>
2023-07-27 22:50:02 +0300

    add wiremock as integration test mock server

--- src/test/java/com/example/gitservice/integration/GitServiceIntegrationTest.java
+++ src/test/java/com/example/gitservice/integration/GitServiceIntegrationTest.java
@@ -5,12 +5,9 @@ import com.example.gitservice.dto.CommitDTO;
 import com.example.gitservice.dto.OwnerDTO;
 import com.example.gitservice.dto.RepositoryDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,86 +18,84 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.IOException;
 import java.util.Arrays;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathMatching;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 class GitServiceIntegrationTest {
 
-    public static MockWebServer mockServer;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeAll
-    static void beforeAll() throws IOException {
-        mockServer = new MockWebServer();
-        mockServer.start();
-    }
+    private WireMockServer wireMockServer;
 
     @DynamicPropertySource
     static void backendProperties(DynamicPropertyRegistry registry) {
-        registry.add("external.resource.github", () -> mockServer.url("/").toString());
+        registry.add("external.resource.github", () -> "http://localhost:8888");
     }
 
-    @AfterAll
-    static void afterAll() throws IOException {
-        mockServer.shutdown();
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer(options().port(8888));
+        wireMockServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
     }
 
     @Test
     void shouldReturnReposSuccessfully() throws Exception {
         RepositoryDTO repositoryDTO = new RepositoryDTO("name", false, new OwnerDTO("login"));
         String responseBody = objectMapper.writeValueAsString(Arrays.asList(repositoryDTO));
-        mockExternalEndpoint(200, responseBody);
+
+        wireMockServer.stubFor(get(urlPathMatching("/users/([A-Za-z]+)/repos.*"))
+                .withQueryParam("per_page", equalTo("100"))
+                .withQueryParam("page", equalTo("1"))
+                .willReturn(ok()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(responseBody)));
 
         BranchDTO branchDTO = new BranchDTO("master", new CommitDTO("ffdg"));
         String responseBodyBranch = objectMapper.writeValueAsString(Arrays.asList(branchDTO));
-        mockExternalEndpoint(200, responseBodyBranch);
+
+        wireMockServer.stubFor(get(urlPathMatching("/repos/([A-Za-z]+)/([A-Za-z]+)/branches.*"))
+                .willReturn(ok()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(responseBodyBranch)));
 
         ResultActions result = executeGetRequest();
 
-        assertBackendServerWasCalledCorrectlyForGET(mockServer.takeRequest());
-        verifyResults(result, 200, "{\"name\":\"name\",\"ownerLogin\":\"login\",\"branches\":[{\"name\":\"master\",\"lastCommitSha\":\"ffdg\"}]}");
+        result.andExpect(status().isOk()).andExpect(content().json("[{\"name\":\"name\",\"ownerLogin\":\"login\",\"branches\":[{\"name\":\"master\",\"lastCommitSha\":\"ffdg\"}]}]"));
     }
 
     @Test
     void shouldReturnNotFound() throws Exception {
-        mockExternalEndpoint(404, "not found");
+
+        wireMockServer.stubFor(get(urlPathMatching("/users/([A-Za-z]+)/repos.*"))
+                .withQueryParam("per_page", equalTo("100"))
+                .withQueryParam("page", equalTo("1"))
+                .willReturn(notFound()));
+
         ResultActions result = executeGetRequest();
 
-        assertBackendServerWasCalledCorrectlyForGET(mockServer.takeRequest());
-        verifyResults(result, 404, "{\"status\":404,\"message\":\"Resource not found\"}");
+        result.andExpect(status().isNotFound()).andExpect(content().json("{\"status\":404,\"message\":\"Resource not found\"}"));
     }
 
     private ResultActions executeGetRequest() throws Exception {
         return mockMvc.perform(MockMvcRequestBuilders
                 .get("/repos").queryParam("username", "user"));
-    }
-
-    private void mockExternalEndpoint(int responseCode, String body) {
-        MockResponse mockResponse = new MockResponse().setResponseCode(responseCode)
-                .setBody(body)
-                .addHeader("Content-Type", "application/json");
-        mockServer.enqueue(mockResponse);
-    }
-
-    private void assertBackendServerWasCalledCorrectlyForGET(RecordedRequest recordedRequest) {
-        assertThat(recordedRequest.getMethod()).isEqualTo("GET");
-        assertThat(recordedRequest.getPath()).isEqualTo("/users/user/repos?per_page=100&page=1");
-    }
-
-    private void verifyResults(ResultActions resultActions, int status, String... message) throws Exception {
-        resultActions
-                .andDo(print())
-                .andExpect(status().is(status));
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        Assertions.assertThat(responseBody).contains(message);
     }
 }
